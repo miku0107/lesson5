b0fe3d9ec9658f4337d55dca47953a36862c4b3b
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-07-30 22:39:39 +0000

    fix_Finish2-5

--- lesson5.rb
+++ lesson5.rb
@@ -1,9 +1,9 @@
-total_price = 150
+total_price = 90
 
 if total_price > 100
     puts "みかんを購入。所持金に余りあり。"
 elsif total_price == 100
     puts "みかんを購入。所持金は0円。"
-elsif total_price < 100
+elsif 
     puts "みかんを購入することができません。"
 end
